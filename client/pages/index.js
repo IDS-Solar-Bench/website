4a6aa2826d93a7055da6b1e7cb3262a3460697da
aamirahmed2004 <syedaamir099@gmail.com>
2024-01-30 01:51:32 -0800

    added client and sample server

---
+++ client/pages/index.js
@@ -0,0 +1,60 @@
+import React, {useEffect, useState} from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+function index() {
+
+  const [timestamp, setTimestamp] = useState("Loading");
+  const [message, setMessage] = useState("Loading");
+  const [temperature, setTemperature] = useState("Loading");
+  const [capacity, setCapacity] = useState("Loading");
+
+  const [readings, setReadings] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/home")
+      .then((response) => response.json())
+      .then((data) =>{
+        setReadings(data.reverse());
+        setTimestamp(data[0].timestamp.substring(12,));
+        setMessage(data[0].message);
+        setTemperature(data[0].temperature);
+        setCapacity(data[0].capacity);
+        console.log(data);
+      });
+  }, []);
+
+  const graphData = readings.map((reading) => ({
+    timestamp: reading.timestamp.substring(12),
+    temperature: reading.temperature,
+  }));
+
+  return (
+    <div>
+      <h1>IDS</h1>
+      <div className="latest-reading">
+        <h2>Latest Reading:</h2>
+        <div>
+          Timestamp: {timestamp},
+          Temp: {temperature}, 
+          Capacity: {capacity},
+          Message: {message}
+        </div>
+      </div>
+      
+      <div className="chart">
+        <ResponsiveContainer width="80%" height={300}>
+          <LineChart data={graphData} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="timestamp" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="temperature" stroke="#ffffff" activeDot={{ r: 8 }} />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
+  );
+}
+
+export default index
---
+++ server/server.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+sample_readings = [
+    {'timestamp': "2024-01-01, 12:00:00", 'temperature': 26, 'capacity': 72, 'message': 'All good'},
+    {'timestamp': "2024-01-01, 11:30:00", 'temperature': 25, 'capacity': 75, 'message': 'Wait actually'},
+    {'timestamp': "2024-01-01, 11:00:00", 'temperature': 28, 'capacity': 77, 'message': 'Nvm'},
+    {'timestamp': "2024-01-01, 10:30:00", 'temperature': 26, 'capacity': 80, 'message': 'No please'},
+    {'timestamp': "2024-01-01, 10:00:00", 'temperature': 25, 'capacity': 82, 'message': 'Get me out'},
+    {'timestamp': "2024-01-01, 09:30:00", 'temperature': 28, 'capacity': 84, 'message': 'I have a wife and kids'},
+    {'timestamp': "2024-01-01, 09:00:00", 'temperature': 26, 'capacity': 86, 'message': 'Warning'},
+    {'timestamp': "2024-01-01, 08:30:00", 'temperature': 25, 'capacity': 88, 'message': 'Normal'},
+    {'timestamp': "2024-01-01, 08:00:00", 'temperature': 28, 'capacity': 90, 'message': 'All good'},
+]
+
+@app.route("/api/home", methods=['GET'])
+def data():
+    return jsonify(sample_readings[-5:])
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
