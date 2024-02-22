8348972e04d1700149b7f3b9cc48dc1cf7ff7c19
Sam <sikora.samj@gmail.com>
2024-02-22 12:36:21 -0800

    Responsive text size

--- client/pages/components/Splash/SplashImage.js
+++ client/pages/components/Splash/SplashImage.js
@@ -5,9 +5,11 @@ import style from "./SplashImage.module.css"
 const SplashImage = () => {
   return (
     <div className={style.container}>
+
+    <div className={style.wrapper}>
         <Image src="/main/temp.svg" alt="Photo of Bench" width={1920} height={1080} />
         <div className={style.splashText + ' customHeadingTextNav'}> Solar Bench Project</div>
-
+    </div>
     </div>
   );
 };
