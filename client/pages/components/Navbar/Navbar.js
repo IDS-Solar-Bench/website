a951b333dc24df0fd130e526f022d9ab7a270855
Sam <sikora.samj@gmail.com>
2024-02-22 11:58:11 -0800

    Image in nav clickable

--- client/pages/components/Navbar/Navbar.js
+++ client/pages/components/Navbar/Navbar.js
@@ -5,11 +5,13 @@ import Image from 'next/image'
 const Navbar = () => {
   return (
     <div className={style.nav}>
-        <a href="https://www.idsubco.com/"> <div className={style.spacerDiv}></div> </a>
+        <div className={style.spacerDiv}></div>
         <div className='customHeadingTextNav'> IDS</div>
-        <div className={style.image}>
-          <Image src="/nav/navLogo.png" alt="IDS" width={3760} height={1928} />
-        </div>
+        <a href="https://www.idsubco.com/"> 
+          <div className={style.image}>
+            <Image src="/nav/navLogo.png" alt="IDS" width={3760} height={1928} />
+          </div>
+        </a>
 
     </div>
   );
