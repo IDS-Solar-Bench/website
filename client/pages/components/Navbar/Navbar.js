081a90fbb765b42a0635074027473319f185870d
Sam <sikora.samj@gmail.com>
2024-02-22 11:35:39 -0800

    Work on Navbar

--- client/pages/components/Navbar/Navbar.js
+++ client/pages/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import Image from 'next/image'
 const Navbar = () => {
   return (
     <div className={style.nav}>
-        <div></div>
+        <a href="https://www.idsubco.com/"> <div className={style.spacerDiv}></div> </a>
         <div className='customHeadingTextNav'> IDS</div>
         <div className={style.image}>
           <Image src="/nav/navLogo.png" alt="IDS" width={3760} height={1928} />
