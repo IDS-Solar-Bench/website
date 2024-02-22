a7fe725087713626dccf390f96e82aee408a2de5
Sam <sikora.samj@gmail.com>
2024-02-22 09:01:02 -0800

    Add Google Fonts and update styling

---
+++ client/pages/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './Navbar.module.css'
+
+const Navbar = () => {
+  return (
+    <div className={style.nav}>
+        <div className='customHeadingText' style={{color: "white", padding: "10px 0px"}}>IDS</div>
+
+    </div>
+  );
+};
+
+export default Navbar;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import Graph from './components/Graph/Graph'
+import Navbar from './components/Navbar/Navbar';
 
 function index() {
 
   return <div>
+    <link href='https://fonts.googleapis.com/css?family=IBM Plex Sans Condensed' rel='stylesheet' />
+
+    <Navbar />
+
+
+    <div className='mainContent'>
+
+      <div className='customHeadingText' style={{color: "#237D5D", padding: "1em 0px"}}> Short Description of Solar Bench </div>
     
-    <div className="foldBlock">
-      <Graph />
+      <div className="foldBlockWrapper">
+        <div className="foldBlock">
+          <Graph />
+        </div>
+      </div>
     </div>
-    
   </div>
 }
 
