8bf6c725dd0328e0f4480c420d9b3913f8976eea
Sam <sikora.samj@gmail.com>
2024-02-22 09:35:07 -0800

    Navbar Work with image

--- client/pages/components/Navbar/Navbar.js
+++ client/pages/components/Navbar/Navbar.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import style from './Navbar.module.css'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
     <div className={style.nav}>
+        <div></div>
         <div className='customHeadingTextNav'> IDS</div>
+        <div className={style.image}>
+          <Image src="/nav/navLogo.png" alt="IDS" width={3760} height={1928} />
+        </div>
 
     </div>
   );
