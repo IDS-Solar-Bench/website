1791f5ba3d00a3cc9bbd625cb81ed34473bfe806
Sam <sikora.samj@gmail.com>
2024-04-02 15:56:14 -0700

    Submit button wrapper

--- client/pages/components/Feedback/Feedback.js
+++ client/pages/components/Feedback/Feedback.js
@@ -27,25 +27,26 @@ const Feedback = () => {
         </div>
 
         <form onSubmit={onSubmit} className={style.form}>
+            <div className={style.inputsWrapper}>
+                <div className={style.nameEmailContainer}>
 
-            <div className={style.nameEmailContainer}>
+                    <div className={style.nameWrapper}>
+                        <label htmlFor="name" className={style.label}> Name </label>
+                        <input type="text" name="name" id="name" className={style.name} placeholder="John Doe" onChange={handleChangeInput}/>
+                    </div>
 
-                <div className={style.nameWrapper}>
-                    <label htmlFor="name" className={style.label}> Name </label>
-                    <input type="text" name="name" id="name" className={style.name} placeholder="John Doe" onChange={handleChangeInput}/>
+                    <div className={style.emailWrapper}>
+                        <label htmlFor="email" className={style.label}> Email </label>
+                        <input type="email" name="email" id="email" className={style.email} placeholder='johnDoe@student.ubc.ca' onChange={handleChangeInput}/>
+                    </div>
                 </div>
 
-                <div className={style.emailWrapper}>
-                    <label htmlFor="email" className={style.label}> Email </label>
-                    <input type="email" name="email" id="email" className={style.email} placeholder='johnDoe@student.ubc.ca' onChange={handleChangeInput}/>
+                <div className={style.messageWrapper}>
+                    <label htmlFor="message" className={style.label}> Message * </label>
+                    <textarea name="message" id="message" required className={style.message} placeholder="One suggestion about the bench I had..." onChange={handleChangeInput}/>
                 </div>
-            </div>
 
-            <div className={style.messageWrapper}>
-                <label htmlFor="message" className={style.label}> Message * </label>
-                <textarea name="message" id="message" required className={style.message} placeholder="One suggestion about the bench I had..." onChange={handleChangeInput}/>
             </div>
-
             <button type="submit" className={style.submitButton}> Submit </button>
 
         </form>
